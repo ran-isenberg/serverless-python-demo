974801deea7533aaba8b8dca24ff95f8e168f98a
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-08-03 16:19:00 +0300

    a

--- tests/integration/test_create_order.py
+++ tests/integration/test_create_order.py
@@ -47,6 +47,7 @@ def test_internal_server_error():
     response = call_create_order(generate_api_gw_event(body.model_dump()))
     assert response['statusCode'] == HTTPStatus.INTERNAL_SERVER_ERROR
     stubber.deactivate()
+    DynamoDalHandler._instances = {}
 
 
 def test_handler_bad_request():
