956a2e155ee0a5c252cd4431d102aac3c7875bf1
heitorlessa <lessa@amazon.co.uk>
2023-09-30 18:53:27 +0200

    chore(domain): create skeleton to notify updates
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- product/models/products/product.py
+++ product/models/products/product.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Literal
 
 from pydantic import BaseModel, Field, PositiveInt
 from pydantic.functional_validators import AfterValidator
@@ -9,8 +9,16 @@ ProductId = Annotated[
     str, Field(min_length=36, max_length=36), AfterValidator(validate_product_id)
 ]
 
+ProductName = Annotated[str, Field(min_length=1, max_length=30)]
+
 
 class Product(BaseModel):
-    name: Annotated[str, Field(min_length=1, max_length=30)]
+    name: ProductName
     id: ProductId
     price: PositiveInt
+
+
+class ProductNotification(BaseModel):
+    product_id: ProductId
+    product_name: ProductName
+    change_status: Literal['ADDED', 'REMOVED', 'UPDATED']
---
+++ product/stream_processor/domain_logic/product_notification.py
@@ -0,0 +1,12 @@
+from typing import TypeVar
+
+from product.models.products.product import ProductNotification
+
+# Until DAL gets created
+EventHandler = TypeVar('EventHandler')
+
+
+def notify_product_updates(
+    update: list[ProductNotification], event_handler: EventHandler
+):
+    ...
