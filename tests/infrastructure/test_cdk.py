9b9719217924c8446214161bc9ff2b9931561668
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-10-18 17:29:55 +0300

    fix infra test

--- tests/infrastructure/test_cdk.py
+++ tests/infrastructure/test_cdk.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import App
 from aws_cdk.assertions import Template
 
@@ -7,7 +9,11 @@ from infrastructure.product.service_stack import ServiceStack
 def test_synthesizes_properly():
     app = App()
 
-    service_stack = ServiceStack(app, 'service-test')
+    service_stack = ServiceStack(
+        app,
+        'service-test',
+        os.getenv('ENVIRONMENT', 'dev'),
+    )
 
     # Prepare the stack for assertions.
     template = Template.from_stack(service_stack)
