74b52d706b42b9ddc20f8be205f78efbf8775aac
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-10-19 18:28:08 +0300

    fix infra tests

--- tests/infrastructure/test_cdk.py
+++ tests/infrastructure/test_cdk.py
@@ -1,5 +1,3 @@
-import os
-
 from aws_cdk import App
 from aws_cdk.assertions import Template
 
@@ -8,14 +6,12 @@ from infrastructure.product.service_stack import ServiceStack
 
 def test_synthesizes_properly():
     app = App()
-    environment = os.getenv('ENVIRONMENT', 'dev'),
-    is_production = True if environment == 'production' else False,
-    service_stack = ServiceStack(app, 'service-test', is_production)
+    service_stack = ServiceStack(scope=app, id='service-test', is_production=True)
 
     # Prepare the stack for assertions.
     template = Template.from_stack(service_stack)
 
     # verify that we have one API GW, that is it not deleted by mistake
     template.resource_count_is('AWS::ApiGateway::RestApi', 1)
-    template.resource_count_is('AWS::DynamoDB::Table', 3)  # main db and one for idempotency and one for tests
+    template.resource_count_is('AWS::DynamoDB::Table', 2)  # main db and one for idempotency
     template.resource_count_is('AWS::Events::EventBus', 1)
