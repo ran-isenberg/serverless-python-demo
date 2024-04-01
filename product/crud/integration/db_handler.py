ce670f023cb87e3ac49d66deb3b9789fca431862
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2024-04-01 17:46:01 +0300

    chore: update deps

--- product/crud/integration/db_handler.py
+++ product/crud/integration/db_handler.py
@@ -15,17 +15,13 @@ class _SingletonMeta(ABCMeta):
 
 class DbHandler(ABC, metaclass=_SingletonMeta):
     @abstractmethod
-    def create_product(self, product: Product) -> None:
-        ...  # pragma: no cover
+    def create_product(self, product: Product) -> None: ...  # pragma: no cover
 
     @abstractmethod
-    def get_product(self, product_id: str) -> Product:
-        ...  # pragma: no cover
+    def get_product(self, product_id: str) -> Product: ...  # pragma: no cover
 
     @abstractmethod
-    def delete_product(self, product_id: str) -> None:
-        ...  # pragma: no cover
+    def delete_product(self, product_id: str) -> None: ...  # pragma: no cover
 
     @abstractmethod
-    def list_products(self) -> List[Product]:
-        ...  # pragma: no cover
+    def list_products(self) -> List[Product]: ...  # pragma: no cover
